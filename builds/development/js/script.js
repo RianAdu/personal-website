6da6b2d4e7be528ea2fe32fc8eb172b11553e6a8
Rian <rian_adu@icloud.com>
2016-04-20 20:54:30 +0200

    added mobile menu

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,5 +1,12 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+//require jQuery
 var $ = require('jquery');
+
+//show mobile Menu function
+$('#menuIcon').on('click', function(){
+	$('nav ul').slideToggle('slow');
+});
+
 /*! picturefill - v3.0.2 - 2016-02-12
  * https://scottjehl.github.io/picturefill/
  * Copyright (c) 2016 https://github.com/scottjehl/picturefill/blob/master/Authors.txt; Licensed MIT
--- components/scripts/rian.js
+++ components/scripts/rian.js
@@ -1,1 +1,7 @@
+//require jQuery
 var $ = require('jquery');
+
+//show mobile Menu function
+$('#menuIcon').on('click', function(){
+	$('nav ul').slideToggle('slow');
+});
