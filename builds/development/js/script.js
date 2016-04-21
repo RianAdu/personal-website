14102f40bfffce381890e72b07370c6812290b24
Rian <rian_adu@icloud.com>
2016-04-21 17:52:41 +0200

    changed header bg image, header text, and colors.

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -2,9 +2,15 @@
 //require jQuery
 var $ = require('jquery');
 
+//show window width
+$(window).on('resize', function(){
+	var width = $(window).width();
+	$('.showSize').text(width);
+});
+
 //show mobile Menu function
 $('#menuIcon').on('click', function(){
-	$('nav ul').slideToggle('slow');
+	$('nav ul').slideToggle();
 });
 
 /*! picturefill - v3.0.2 - 2016-02-12
--- components/scripts/rian.js
+++ components/scripts/rian.js
@@ -1,7 +1,13 @@
 //require jQuery
 var $ = require('jquery');
 
+//show window width
+$(window).on('resize', function(){
+	var width = $(window).width();
+	$('.showSize').text(width);
+});
+
 //show mobile Menu function
 $('#menuIcon').on('click', function(){
-	$('nav ul').slideToggle('slow');
+	$('nav ul').slideToggle();
 });
