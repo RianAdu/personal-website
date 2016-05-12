70837b9047cc8505e5e4ee9fdc2a900f6c4d1723
Rian <rian_adu@icloud.com>
2016-05-13 01:28:28 +0200

    added fixed navigation | animated scrolling and fixt minior things

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -3,12 +3,13 @@ var $ = require('jquery'); //get jQuery
 var picturefill = require('picturefill'); //get responsive image polyfill
 
 //rian adu script
-$(document).ready(function(){
+$(function(){
+
 	//show mobile Menu function
 	$('#mobileMenuIcon').on('click', function(){
 		$('#mobileDropDown').slideToggle();
 	});
-	
+
 	var setCopyYear = function(){
 		var date = new Date();
 		var thisYear = date.getFullYear();
@@ -29,6 +30,7 @@ $(document).ready(function(){
 	});
 
 	//form validation
+	
 	var formValidation = function(){
 		$("#contactForm").validate({
 			errorPlacement: function(error, element){
@@ -45,11 +47,52 @@ $(document).ready(function(){
 		});
 	};
 
+	var animatedScrolling = function(duration){
+		var navbarHeight = $('#navbar').height();
+		$('a[href^="#"]:not(#enterButton)').on('click', function(e){
+			$('#mobileDropDown').slideUp(); //close the menu
+			var target = $( $(this).attr('href') );
+				if(target.length){
+					e.preventDefault();
+
+					$('html, body').animate({
+						scrollTop: target.offset().top-navbarHeight
+					}, duration);
+				}
+		});
+	};
+
+	var stickyNav = function(){
+
+		var navbar = $('nav');
+		var navPos = navbar.offset().top;
+
+		navbar.wrap('<div class="navPlaceholder"></div>"');
+		$('.navPlaceholder').height($('#navbar').outerHeight());
+
+		$(window).scroll(function(){
+			var scrollPos = $(window).scrollTop();
+
+			if(scrollPos >= navPos){
+				navbar.addClass('fixed');
+			}
+			else{
+				navbar.removeClass('fixed');
+			}
+		});
+		//set placeholder height on window resize again
+		$(window).resize(function(){
+			$('.navPlaceholder').height($('#navbar').outerHeight());
+		});
+
+	};
+	
 	headerwHeight();
 	setCopyYear();
 	formValidation();
+	animatedScrolling(1100);
+	stickyNav();
 });
-
 	
 /*!
  * jQuery Validation Plugin v1.15.0
