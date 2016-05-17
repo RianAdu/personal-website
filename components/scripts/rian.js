34089c12f8189f28d55120df7b3934135b3841bb
Rian <rian_adu@icloud.com>
2016-05-17 12:48:58 +0200

    changed and navigation bar to fix the iOS fixed navbar issue | cleaned the javascript code a bit

--- components/scripts/rian.js
+++ components/scripts/rian.js
@@ -1,35 +1,93 @@
 var $ = require('jquery'); //get jQuery
 var picturefill = require('picturefill'); //get responsive image polyfill
 
-//rian adu script
+//RianAdu functions
 $(function(){
 
-	//show mobile Menu function
-	$('#mobileMenuIcon').on('click', function(){
-		$('#mobileDropDown').slideToggle();
-	});
+	var init = function(){
+		showWidth();
+		mobileMenu();
+		introHeight();
+		setCopyYear();
+		formValidation();
+		smoothScrolling(1100);
+		setDarkNav();
+	}; // end of init
 
+	var showWidth = function(){
+		$(window).on('resize', function(){
+			var width = $(window).width();
+			$('.showSize').text(width);
+			introHeight();
+		});
+	}; // end of showWidth
+
+
+	var introHeight = function(){
+		var height = $(window).height();
+		$('header article').css('height', height);
+	}; // end of introHeight
+
+
+	var mobileMenu = function(){
+		$('#mobileMenuIcon').on('click', function(){
+			
+			//only add class transtoDark if user is still on top of the page
+			if($('nav').hasClass('darkNav')){
+				$('#mobileDropDown').slideToggle();
+			}
+			else {
+				$('nav').toggleClass('transToDark');
+				$('#mobileDropDown').slideToggle();
+			}
+		});
+	}; //end of mobileMenu
+
+	var smoothScrolling = function(duration){
+		var navbarHeight = $('#navbar').height();
+		
+		$('a[href^="#"]:not(#enterButton)').on('click', function(e){
+			$('#mobileDropDown').slideUp(); //close the menu
+			$('.navButton').removeClass('active');
+
+			var target = $( $(this).attr('href') );
+					
+			if(target.length){
+				e.preventDefault();
+
+				$('html, body').animate({
+					scrollTop: target.offset().top-navbarHeight
+				}, duration);
+
+				$(this).addClass('active');
+			}
+		});
+	}; // end of  smoothScrolling
+
+	var setDarkNav = function(){
+		var intro = $('#hgroup');
+		var navbar = $('nav');
+		var introPos = intro.offset().top;
+
+		$(window).scroll(function(){
+			var scrollPos = $(window).scrollTop();
+
+			if(scrollPos >= introPos){
+				navbar.addClass('darkNav');
+			}
+			else{
+				navbar.removeClass('darkNav transToDark');
+			}
+		});
+	}; // end of setDarkNav
+	
 	var setCopyYear = function(){
 		var date = new Date();
 		var thisYear = date.getFullYear();
 		$('#thisYear').text(thisYear);
-	};
+	}; // end of setCopyYear
 
-	//headerImage size
-	var headerwHeight = function(){
-		var height = $(window).height();
-		$('header article').css('height', height);
-	};
 
-	//show window width
-	$(window).on('resize', function(){
-		var width = $(window).width();
-		$('.showSize').text(width);
-		headerwHeight();
-	});
-
-	//form validation
-	
 	var formValidation = function(){
 		$("#contactForm").validate({
 			errorPlacement: function(error, element){
@@ -44,49 +102,8 @@ $(function(){
 				}
 			}
 		});
-	};
-
-	var animatedScrolling = function(duration){
-		var navbarHeight = $('#navbar').height();
-		
-		$('a[href^="#"]:not(#enterButton)').on('click', function(e){
-			$('#mobileDropDown').slideUp(); //close the menu
-			var target = $( $(this).attr('href') );
-				if(target.length){
-					e.preventDefault();
-
-					$('html, body').animate({
-						scrollTop: target.offset().top-navbarHeight
-					}, duration);
-				}
-		});
-	};
-
-	var stickyNav = function(){
-		var navbar = $('nav');
-		var navPos = navbar.offset().top;
-		$('.navPlaceholder').height($('#navbar').outerHeight());
-
-		$(window).scroll(function(){
-			var scrollPos = $(window).scrollTop();
-
-			if(scrollPos >= navPos){
-				navbar.addClass('fixed');
-			}
-			else{
-				navbar.removeClass('fixed');
-			}
-		});
-		//set placeholder height on window resize again
-		$(window).resize(function(){
-			$('.navPlaceholder').height($('#navbar').outerHeight());
-		});
-	};
+	}; // end of form validation
 	
-	headerwHeight();
-	setCopyYear();
-	formValidation();
-	animatedScrolling(1100);
-	stickyNav();
+	init();
 });
 	
