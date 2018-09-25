44f60df02a5a5d96f7e4bf9b00de8e349e1d2ccf
Florian <rian_adu@icloud.com>
2018-09-24 22:52:04 -0400

    Set the project detail descrition to left aligned

--- components/scripts/inc/project-template.js
+++ components/scripts/inc/project-template.js
@@ -1,5 +1,5 @@
 var projectTemplate = '<div class="portfolio-details__overlay"><article class="portfolio-details__wrapper"><section class="portfolio-details"><div class="container">'+
-'<div class="row"><div class="portfolio-details__description col-xs-12 col-md-10 col-md-offset-1 text-center"><span class="portfolio-details__close-button lnr lnr-cross"></span>'+
+'<div class="row"><div class="portfolio-details__description col-xs-12 col-md-9"><span class="portfolio-details__close-button lnr lnr-cross"></span>'+
 '<h2 class="portfolio-details__description-title">{{{title}}}</h2>{{{copy}}}'+
 '<h3 class="portfolio-details__description-subheader">Technologies</h3><p class="portfolio-details__description-technologies">{{#technologies}}'+
 '{{.}}{{/technologies}}</p></div> </div> </div><div class="portfolio-details__images-wrapper"><div class="container"><div class="row">'+
