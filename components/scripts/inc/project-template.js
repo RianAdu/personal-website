4fec86f94ff83c33b9fc9801db9661adb94b9d2c
Florian <rian_adu@icloud.com>
2018-09-24 23:42:07 -0400

    Added tech-icons to portfolio details view and renamed svg-files

--- components/scripts/inc/project-template.js
+++ components/scripts/inc/project-template.js
@@ -1,8 +1,7 @@
 var projectTemplate = '<div class="portfolio-details__overlay"><article class="portfolio-details__wrapper"><section class="portfolio-details"><div class="container">'+
 '<div class="row"><div class="portfolio-details__description col-xs-12 col-md-9"><span class="portfolio-details__close-button lnr lnr-cross"></span>'+
 '<h2 class="portfolio-details__description-title">{{{title}}}</h2>{{{copy}}}'+
-'<h3 class="portfolio-details__description-subheader">Technologies</h3><p class="portfolio-details__description-technologies">{{#technologies}}'+
-'{{.}}{{/technologies}}</p></div> </div> </div><div class="portfolio-details__images-wrapper"><div class="container"><div class="row">'+
+'<h3 class="portfolio-details__description-subheader">Technologies</h3>{{{setTechnologies}}}</div> </div> </div><div class="portfolio-details__images-wrapper"><div class="container"><div class="row">'+
 '<div class="portfolio-details__images col-xs-12">{{{getImages}}}<div class="portfolio-details__button-wrapper">'+
 '<a href="{{{url}}}" target="_blank" class="portfolio-details__button portfolio-details__button--first btn-custom btn">Visit website</a>'+
 '<button class="portfolio-details__button portfolio-details__button--close btn-custom btn"> Close project</button></div></div></div></div></div></section> </article></div>';
