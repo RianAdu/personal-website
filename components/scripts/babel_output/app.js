855cb0d183a954962836453a825581c7b891f4ef
Florian <rianadu416@gmail.com>
2020-05-29 00:32:47 -0400

    Removed Netlify reCapctha and added hidden text field for SPAM filter

--- components/scripts/babel_output/app.js
+++ components/scripts/babel_output/app.js
@@ -28,6 +28,7 @@ $(function () {
       mobileNav: null,
       showMoreButton: null,
       contactForm: null,
+      hiddenField: null,
       submitModal: null,
       formGroups: null,
       formInputs: null,
@@ -59,6 +60,7 @@ $(function () {
       App.dom.contactForm = $('#contact-form');
       App.dom.formGroups = $('.form-group');
       App.dom.formInputs = $('.form-control');
+      App.dom.hiddenField = $('.contact__form--custom-field');
       App.dom.submitModal = $('#myModal');
       App.dom.pageInitOverlay = $('.page_init_overlay');
     },
@@ -244,7 +246,7 @@ $(function () {
       // this function scrolls the body back to the last clicked Project, after the overlay has been closed
       $('html, body').scrollTop($('#' + viewedProject + '').offset().top - (App.variables.navbarHeight - 1));
     },
-    //resetting the form input after submit
+    //resetting the form input after submit or when a SPAM bot was trying to submit
     resetContactForm: function resetContactForm() {
       App.dom.contactForm.trigger('reset');
       App.dom.formGroups.each(function () {
@@ -274,9 +276,15 @@ $(function () {
         },
         submitHandler: function submitHandler(form, e) {
           e.preventDefault();
-          $.post(App.dom.contactForm.attr("action"), App.dom.contactForm.serialize()).then(function () {
-            App.dom.submitModal.modal();
-          });
+
+          if (App.dom.hiddenField.val().length > 0) {
+            App.resetContactForm();
+            return false;
+          } else {
+            $.post(App.dom.contactForm.attr("action"), App.dom.contactForm.serialize()).then(function () {
+              App.dom.submitModal.modal();
+            });
+          }
         }
       });
     }
